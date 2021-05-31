7d70f8887d768b7b18bdc3d52a4b8326f08bd032
Ash-ishkhatri <ak27021319@student.ku.edu.np>
2021-05-31 22:06:37 +0545

    nothing

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'ku_quora',
     'account',
     'notification',
+
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
