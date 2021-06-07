94d90ff7f61cdd362921f90a45355e238c66fc02
Ash-ishkhatri <ak27021319@student.ku.edu.np>
2021-06-07 16:15:19 +0545

    nothing

--- staticfiles/ku_quora/add-question.js
+++ staticfiles/ku_quora/add-question.js
@@ -1,26 +1,36 @@
+
 try{
     addQuestion = document.querySelector('.add-question');
+    triggerArrow = document.querySelector('.addQuestion-container i.fa-angle-double-down');
+
     addQuestionForm = document.querySelector('.add-question-form');
     form = document.querySelector('.add-question-form form');
     
-    addQuestion.addEventListener('click',()=>{
+    triggerArrow.addEventListener('click',formApperance)
+    addQuestion.addEventListener('click',formApperance)
+
+
+    function formApperance(){
         if(addQuestionForm.dataset.status == 'collapsed'){
             addQuestionForm.dataset.status = 'expanded';
             addQuestionForm.style.height = addQuestionForm.scrollHeight + 'px';
-            addQuestionForm.style.transform = 'scaleY(1)';
-
-                form.style.opacity = '1';
-
+            // addQuestionForm.style.transform = 'scaleY(1)';
+            triggerArrow.classList.remove('animate-direction-down');
+            triggerArrow.classList.add('animate-direction-up');
+            console.log('clicked')
+                // form.style.opacity = '1';
+            
             }
             else{
                 addQuestionForm.dataset.status = 'collapsed';
                 addQuestionForm.style.height = '0px';
-                addQuestionForm.style.transform = 'scaleY(0)';
-
-                    form.style.opacity = '0';
+                // triggerArrow.style.transform = 'rotate(0deg)';
+                triggerArrow.classList.remove('animate-direction-up');
+                triggerArrow.classList.add('animate-direction-down');
 
             }
-        })
+    }
+
 
     
 }catch(err){
