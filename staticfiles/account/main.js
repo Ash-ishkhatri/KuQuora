50f111277d7be82ebce7a98f54dfd5dd85f2bdfb
Ash-ishkhatri <ak27021319@student.ku.edu.np>
2021-06-09 09:14:48 +0545

    nothing

--- staticfiles/account/main.js
+++ staticfiles/account/main.js
@@ -9,23 +9,11 @@ if(window.location.pathname == '/account/register/'){
     password1 = document.querySelector(' input[name="password1"]');
     password2 = document.querySelector(' input[name="password2"]');
     let validForm = 0;
-    let nameFormat = /^[a-z ,.'-]+$/i;
+    // let nameFormat = /^[a-z ,.'-]+$/i;
     let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
     let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
 
     signupBtn.classList.add('inactive');
-
-    // function validateName(e){
-    //     if(!nameFormat.test(e.value)){
-    //         e.target.style.border = "2px solid red";
-    //     }else{
-    //         e.target.style.border = "2px solid green";
-    //     }
-    // }
-
-
-    // firstName.addEventListener('keyup',validateName);
-    // lastName.addEventListener('keyup',validateName);
 
 
     usernameField.addEventListener('keyup',()=>{
