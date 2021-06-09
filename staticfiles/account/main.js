06e7b6936f99258bc8dc8cb5c380ac60b8439502
Ash-ishkhatri <ak27021319@student.ku.edu.np>
2021-06-09 13:49:35 +0545

    nothing

--- staticfiles/account/main.js
+++ staticfiles/account/main.js
@@ -2,13 +2,14 @@ if(window.location.pathname == '/account/register/'){
     firstName = document.querySelector(' input[name="first_name"]');
     lastName = document.querySelector(' input[name="last_name"]');
 
+    form = document.querySelector('form#signup')
     usernameField = document.querySelector(' input[name="username"]');
     usernameField = document.querySelector(' input[name="username"]');
     emailField = document.querySelector(' input[name="email"]');
     signupBtn = document.querySelector(' input[type="submit"]');
     password1 = document.querySelector(' input[name="password1"]');
     password2 = document.querySelector(' input[name="password2"]');
-    let validForm = 0;
+    let validEmail = validUsername = validPassword1 =  validPassword2 = 0;
     // let nameFormat = /^[a-z ,.'-]+$/i;
     let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
     let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
@@ -33,11 +34,11 @@ if(window.location.pathname == '/account/register/'){
             if(response.taken == true){
                 field.style.border = '2px solid red';
                 field.parentNode.children[2].style.opacity = "1";
-                validForm = 0;
+                validUsername = 0;
             }else if(response.taken == false && field.value.length > 3){
                 field.style.border = '2px solid rgb(82, 202, 13)';
                 field.parentNode.children[2].style.opacity = "0";
-                validForm = 1;
+                validUsername = 1;
             }
         }
 
@@ -47,7 +48,7 @@ if(window.location.pathname == '/account/register/'){
 
         xhr.send(JSON.stringify(data));
         
-        
+        signupBtnActivation();
 
 
     })
@@ -72,12 +73,14 @@ if(window.location.pathname == '/account/register/'){
             if(response.taken == true){
                 field.style.border = '2px solid red';
                 field.parentNode.children[2].style.opacity = "1";
-                validForm = 0;
+                validEmail = 0;
             }else if(response.taken == false && field.value.match(mailFormat)){
                 field.style.border = '2px solid rgb(82, 202, 13)';
                 field.parentNode.children[2].style.opacity = "0";
-                validForm = 1;
+                validEmail = 1;
             }
+            signupBtnActivation();
+
         }
 
         data = {
@@ -100,21 +103,25 @@ if(window.location.pathname == '/account/register/'){
             field.parentNode.children[4].style.opacity = "1";
             field.parentNode.children[2].style.opacity = "0";
             field.parentNode.children[3].style.opacity = "0";
-            console.log('strong')
+            console.log('strong');
+            validPassword1 = 1;
         }else if(mediumPassword.test(pwd)){
             field.style.border = "2px solid rgb(233, 136, 26)";
             field.parentNode.children[3].style.opacity = "1";
             field.parentNode.children[2].style.opacity = "0";
             field.parentNode.children[4].style.opacity = "0";
-            console.log('medium')
+            console.log('medium');
+            validPassword1 = 1;
         }else{
             field.style.border = "2px solid red";
             field.parentNode.children[2].style.opacity = "1";
             field.parentNode.children[3].style.opacity = "0";
             field.parentNode.children[4].style.opacity = "0";
             console.log('weak');
-
+            validPassword1 = 0;
         }
+        signupBtnActivation();
+
     })
 
     password2.addEventListener('keyup',()=>{
@@ -124,20 +131,41 @@ if(window.location.pathname == '/account/register/'){
         if(password1.value != pwd){
             field.style.border = "2px solid red";
             field.parentNode.children[2].style.opacity = "1";
-            validForm = 0;
+            validPassword2 = 0;
         }else{
             field.style.border = "2px solid rgb(82,202,13)";
             field.parentNode.children[2].style.opacity = "0";
-            validForm = 1;
+            validPassword2 = 1;
+        
         }
+        signupBtnActivation();
+
     })
 
-    password2.addEventListener('focusout',()=>{
-        if(!validForm){
-            signupBtn.classList.add('inactive');
-        }else{
-            if(signupBtn.classList.contains('inactive'))
+
+    function signupBtnActivation(){
+        console.log('inside')
+            console.log(validUsername , validPassword1, validEmail,validPassword2)
+    
+            if(!(validUsername && validEmail && validPassword1 && validPassword2)){
+              console.log('1');
+                if(!signupBtn.classList.contains('inactive')) signupBtn.classList.add('inactive');
+                event.preventDefault();
+           
+            }else{
+              console.log('2');
+    
                 signupBtn.classList.remove('inactive');
-        }
-    })
+                return true;
+    
+            }
+   
+    }
+
+    // emailField.addEventListener('focusout',signupBtnActivation);
+    // usernameField.addEventListener('focusout',signupBtnActivation);
+    // password1.addEventListener('focusout',signupBtnActivation);
+    // password2.addEventListener('focusout',signupBtnActivation);
+
+
 }
